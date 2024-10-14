b1477530cb2f19f7d5fb999f39bc23eb04e64d18
vctrubio <vctrubio@gmail.com>
2024-10-14 16:19:36 +0200

    v2.2 courasel effect

--- src/components/slideSwiper.js
+++ src/components/slideSwiper.js
@@ -45,6 +45,7 @@ export const SlideSwiper = ({ imgs }) => {
               <GatsbyImage
                 image={getImage(img)}
                 alt={`Slide ${index}`}
+                lazy={true}
                 style={{
                   objectFit: 'cover',
                   width: '100%',
@@ -69,30 +70,32 @@ export const SlideSwiper = ({ imgs }) => {
           onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
         >
           {imgs.map((img, index) => (
-            <SwiperSlide
-              key={index}
-              onClick={() => handleClick(index)}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: '100%',
-              }}
-            >
-              <GatsbyImage
-                image={getImage(img)}
-                alt={`Slide ${index}`}
-                lazy={true}
-                style={{
-                  verticalAlign: 'baseline',
-                  height: activeIndex === index ? '50vh' : '100%',
-                  width: activeIndex === index ? '100%' : '80%',
-                  objectFit: 'cover',
-                  opacity: activeIndex === index ? 1 : 0.2,
-                  transform: activeIndex === index ? 'scale(1.5)' : 'scale(0.3)',
-                }}
-              />
-            </SwiperSlide>
+           <SwiperSlide
+           key={index}
+           onClick={() => handleClick(index)}
+           style={{
+             display: 'flex',
+             alignItems: 'center',
+             justifyContent: 'center',
+             height: '100%',
+             transition: 'transform 0.3s ease-in-out', // Smooth transition for scaling
+           }}
+         >
+           <GatsbyImage
+             image={getImage(img)}
+             alt={`Slide ${index}`}
+             lazy={true}
+             style={{
+               verticalAlign: 'baseline',
+               objectFit: 'cover',
+               opacity: activeIndex === index ? 1 : 0.3,  // Decrease opacity for non-active images
+               width: activeIndex === index ? '100%' : '80%',  // Make active image wider
+               height: activeIndex === index ? '65vh' : '50vh',  // Make active image taller
+               transform: activeIndex === index ? 'scale(1)' : 'scale(0.8)',  // Scale active image larger
+               transition: 'all 0.3s ease', // Smooth transition for width, height, opacity, and scale
+             }}
+           />
+         </SwiperSlide>
           ))}
         </Swiper>
       )}
