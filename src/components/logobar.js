0be662035f87965676cca6e521fc2545fca54f36
vctrubio <vctrubio@gmail.com>
2026-01-13 17:12:45 +0100

    navbar padding

--- src/components/logobar.js
+++ src/components/logobar.js
@@ -1,27 +1,30 @@
-import React from "react";
-import { navigate } from "gatsby";
-import logo from "../images/logoAA.webp";
+import React from "react"
+import { navigate } from "gatsby"
+import logo from "../images/logoAA.webp"
 
 export const LogoBar = () => {
-    const handleClick = () => {
-        navigate('/');
-    };
-  
-    return (
-      <div className="d-flex justify-content-around" onClick={handleClick} style={{width: '100%'}}>
-        <img
-          src={logo}
-          alt="Alicia Agosti Interiorismo"
-          width="420"
-          height="auto" // Maintain aspect ratio
-          style={{
-            width: '300px',
-            marginTop: '2em',
-            transform: 'scale(1.4)',
-            cursor: 'pointer' // Added since it has a click handler
-          }}
-        />
-      </div>
-    );
-  };
-  
+  const handleClick = () => {
+    navigate("/")
+  }
+
+  return (
+    <div
+      className="d-flex justify-content-around"
+      onClick={handleClick}
+      style={{ width: "100%" }}
+    >
+      <img
+        src={logo}
+        alt="Alicia Agosti Interiorismo"
+        width="420"
+        height="auto" // Maintain aspect ratio
+        style={{
+          width: "420ox",
+          marginTop: "2em",
+          transform: "scale(1.4)",
+          cursor: "pointer", // Added since it has a click handler
+        }}
+      />
+    </div>
+  )
+}
